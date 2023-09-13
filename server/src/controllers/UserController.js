afb738b8acc6e0cd74371bfc47dfe64ca5213a1c
nattaponkum <nattaponk@nu.ac.th>
2023-09-13 11:57:22 +0700

    page 95

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -1,26 +1,94 @@
+const { User } = require('../models')
+
 module.exports = {
     //get all user
-    index (req,res){
-        res.send('ดูข้อมูลผู้ใช้ทั้งหมด')
+    async index (req,res){
+        // res.send('ดูข้อมูลผู้ใช้ทั้งหมด')
+        try {
+            const users = await User.findAll()
+            res.send(users)
+        } catch (error) {
+            res.status(500).send({
+                error: 'The users information was incorrect'
+            })
+        }
     },
 
     //creat user
-    create (req,res) {
-        res.send('สร้างผู้ใช้' + JSON.stringify(req.body))
+    async create (req,res) {
+        // res.send('สร้างผู้ใช้' + JSON.stringify(req.body))
+        try {
+            const user = await User.create(req.body)
+            res.send(user)
+            
+        } catch (error) {
+            res.status(500).send({
+                error: 'Create users information was incorrect'
+            })
+        }
     },
 
     //edit user
-    put (req, res){
-        res.send('แก้ไขข้อมูลผู้ใช้ ' + req.params.userId + " " + JSON.stringify(req.body.name))
+    async put (req, res){
+        // res.send('แก้ไขข้อมูลผู้ใช้ ' + req.params.userId + " " + JSON.stringify(req.body.name))
+        try {
+            await User.update(req.body,{
+                where: {
+                    id: req.params.userId
+                }
+            })
+            res.send(req.body)
+        } catch (error) {
+            res.status(500).send({
+                error: 'Update user information was incorrect'
+            })
+        }
     },
 
     //delete user
-    delete (req, res){
-        res.send('ลบข้อมูลผู้ใช้ ' + req.params.userId + " " + JSON.stringify(req.body.name))
+    async delete (req, res){
+        // res.send('ลบข้อมูลผู้ใช้ ' + req.params.userId + " " + JSON.stringify(req.body.name))
+        try {
+            const user = await User.findOne({
+                where: {
+                    id: req.params.userId
+                }
+            })
+            if (!user){
+                return res.status(403).send({
+                    error: 'The user id is not found'
+                })
+            }
+            await user.destroy()
+            res.send(user)
+
+        } catch (error) {
+            res.status(500).send({
+                error: 'Delete user information was incorrect'
+            })
+        }
     },
 
     //show user by id
-    show(req,res){
-        res.send('ดูข้อมูลผู้ใช้ '+ req.params.userId + " " + JSON.stringify(req.body))
+    async show(req,res){
+        // res.send('ดูข้อมูลผู้ใช้ '+ req.params.userId + " " + JSON.stringify(req.body))
+        try {
+            const user = await User.findOne({
+                where: {
+                    id: req.params.userId
+                }
+            })
+            if (!user){
+                return res.status(403).send({
+                    error: 'The user id is not found'
+                })
+            }
+            res.send(user)
+        } catch (error) {
+            res.status(500).send({
+                error: 'Users information was incorrect'
+            })
+            
+        }
     }
 }
